0066d10a5da0b3b0664e580dafcaf6e84ef77b41
professorWu <hwu@qcc.cuny.edu>
2024-09-09 12:59:59 -0400

    lab 3 review Python control flow

---
+++ lab3_python/lab3_lastname.py
@@ -0,0 +1,34 @@
+"""
+student's full name
+lab 3, Python review
+"""
+print("--------- Example 1: control flow ----------- ")
+labs = int(input("Enter labs' grade: "))
+exams = int(input("Enter exams' grade: "))
+gpa = ''
+finalgrade = 0
+
+if (0<=labs<=100 and 0<=exams<=100):
+    finalgrade = (labs+exams) / 2
+    if (100>=finalgrade>=90):
+        gpa = 'A'
+    elif (90>finalgrade>=80):
+        gpa = 'B'
+    elif (80>finalgrade>=70):
+        gpa = 'C'
+    elif (70>finalgrade>= 60):
+        gpa = 'D'
+    elif (60>finalgrade>=0):
+        gpa = 'F'
+    else:
+        gpa = 'UNDEFINED'
+else:
+    if not(0<=labs<=100):
+        print(f"Grade for lab {labs} is invalid")
+    elif not(0<=exams<=100):
+        print(f"Grade for exams {exams} is invalid")
+    else:
+        print(f"Grade for labs = {labs} and exams = {exams} are invalid")
+    gpa = 'UNDEFINED'
+
+print(f"Your final grade for the class is {finalgrade} = {gpa}")
