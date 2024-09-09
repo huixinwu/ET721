47e994cfe7caf594d0b8856c212b6e14296dc7f4
professorWu <hwu@qcc.cuny.edu>
2024-09-09 13:28:37 -0400

    example 2 and 3, while loop

--- lab3_python/lab3_lastname.py
+++ lab3_python/lab3_lastname.py
@@ -32,3 +32,39 @@ else:
     gpa = 'UNDEFINED'
 
 print(f"Your final grade for the class is {finalgrade} = {gpa}")
+
+print("--------- Example 2: Loops, guees number ----------- ")
+SECRET = 8
+userguess = int(input("Guess a number between 1 and 10: "))
+while not(SECRET ==userguess):
+    userguess = int(input("wrong guess. Guess again: "))
+
+print(f"Congrats! {userguess} is the right number!")
+
+print("--------- Example 3: Loops, break statement ----------- ")
+balance = 1000
+widthdraw = 0
+deposit = 0
+
+while True:
+    userinput = input("Do you want to widthdraw, w, or deposit ,d? ")
+    if userinput == 'w' or userinput == 'W':
+        w_amount = int(input('How much do you want to widthdraw? '))
+        if w_amount>balance:
+            print(f"Unsuficient funds! You can't widthdraw more than {balance} ")
+        else:
+            balance -= w_amount
+            print(f"Your new balance is {balance}")
+    elif userinput =='d' or userinput == 'D':
+        d_amount = int(input('How much do you want to deposit? '))
+        balance += d_amount
+        print(f"Your new balance is {balance}")
+    else:
+        print("Invalid selection!")
+    
+    choice = input("Would you like to do another transaction? (y,n)")
+    if not(choice=='y' or choice =='Y'):
+        break
+
+print("Thank you for banking with us!")
+    
